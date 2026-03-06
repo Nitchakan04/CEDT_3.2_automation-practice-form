8612b7c47e0b9a059a131a37393449710393f5ef
Nitchakan <nitchakan.kkng@gmail.com>
2026-03-06 08:24:09 +0700

    update test data

--- data/registration.data.ts
+++ data/registration.data.ts
@@ -4,9 +4,7 @@ import type { RegistrationData } from "../pages/RegistrationPage";
 
 export const BASE_URL = "https://demoqa.com/automation-practice-form";
 
-// ==========================
-// File Upload Test Data
-// ==========================
+// File Upload
 export const TEST_FILES = {
   png: path.resolve(process.cwd(), "picture", "sample01.png"),
   jpg: path.resolve(process.cwd(), "picture", "sample02.jpg"),
@@ -14,9 +12,7 @@ export const TEST_FILES = {
   txt: path.resolve(process.cwd(), "picture", "sample04.txt"),
 } as const;
 
-// ==========================
-// Email Test Data
-// ==========================
+// Email
 export const TEST_EMAILS = {
   validBasic: "name@example.com",
   invalidUniversity: "name@student.chula.ac.th",
@@ -24,9 +20,7 @@ export const TEST_EMAILS = {
   invalidMissingTld: "name@example",
 } as const;
 
-// ==========================
-// Mobile Test Data
-// ==========================
+// Mobile 
 export const TEST_MOBILES = {
   valid10: "0812345678",
   invalid9: "081234567",
@@ -34,9 +28,7 @@ export const TEST_MOBILES = {
   invalidSymbols: "0-123@4567",
 } as const;
 
-// ==========================
-// Name Test Data
-// ==========================
+// Name
 export const TEST_NAMES = {
   validFirst: "name",
   validLast: "surname",
@@ -44,32 +36,24 @@ export const TEST_NAMES = {
   special: "N@me",
 } as const;
 
-// ==========================
-// DOB Widget Test Data
-// ==========================
+// DOB Widget
 export const TEST_DOB = {
   example: { day: 5, month: 3, year: 2006 },
 } as const;
 
-// ==========================
-// Subjects Widget Test Data
-// ==========================
+// Subjects Widget
 export const TEST_SUBJECTS = {
   two: ["Maths", "Physics"] as string[],
   three: ["Maths", "Physics", "Chemistry"] as string[],
 };
 
-// ==========================
-// State / City Dynamic Data
-// ==========================
+// State / City 
 export const TEST_STATE_CITY = {
   NCR: { state: "NCR", city: "Delhi" },
   UP: { state: "Uttar Pradesh", city: "Lucknow" },
 } as const;
 
-// ==========================
-// Valid Form Data (used in submit tests)
-// ==========================
+// Valid Form Data
 export const VALID_DATA: RegistrationData = {
   firstName: TEST_NAMES.validFirst,
   lastName: TEST_NAMES.validLast,
@@ -86,9 +70,7 @@ export const VALID_DATA: RegistrationData = {
   city: TEST_STATE_CITY.NCR.city,
 };
 
-// ==========================
 // Required Base Data
-// ==========================
 export const REQUIRED_BASE: RegistrationData = {
   firstName: TEST_NAMES.validFirst,
   lastName: TEST_NAMES.validLast,
